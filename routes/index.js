c8de59cb725ee6a3867352c0e8d7a187dd954c98
Billy Turvey <billy@billyturvey.com>
2021-04-05 18:06:03 +0100

    feat: adds route '/incoming' which logs the request body to the console

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Fermentr' });
+});
+
+
+
+router.post('/incoming', (req, res, next) => {
+  console.log(`🍺  INCOMING...  🌡`);
+  console.log(req.body);
 });
 
 export default router;
