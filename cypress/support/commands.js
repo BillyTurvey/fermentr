a6d450171a7f8ec81dbd0db0fe9f581512c68aa7
Billy Turvey <billy@billyturvey.com>
2021-06-20 18:37:37 +0100

    Feat: Adds logInAs() command to the cypress commands file. This was previously inside a 'testUtils.js' file, all functionality should be moved over to the commands file.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import jeanette from '../fixtures/testUserJeanette.json';
+import nelson from '../fixtures/testUserNelson.json';
+
+Cypress.Commands.add('logInAs', userObject => {
+	if (user === 'Jeanette') user = jeanette;
+	if (user === 'Nelson') user = nelson;
+	cy.request({
+		method: 'POST',
+		url: '/user/logIn',
+		body: {
+			email: user.email,
+			password: user.password
+		}
+	});
+});
