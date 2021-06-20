8297bbab8784dc26b36ad04055ffe444f331cda7
Billy Turvey <billy@billyturvey.com>
2021-06-20 18:51:20 +0100

    Feat: Adds deleteFermentation(fermentation, fermentationOwner) command to cypress commands file. It can be used without passing a user in if the command is used while the user who owns the fermentation is already logged in.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 import jeanette from '../fixtures/testUserJeanette.json';
 import nelson from '../fixtures/testUserNelson.json';
 
-Cypress.Commands.add('logInAs', userObject => {
+Cypress.Commands.add('logInAs', user => {
 	if (user === 'Jeanette') user = jeanette;
 	if (user === 'Nelson') user = nelson;
 	cy.request({
@@ -13,3 +13,30 @@ Cypress.Commands.add('logInAs', userObject => {
 		}
 	});
 });
+
+Cypress.Commands.add('logOut', () => cy.request('POST', '/user/logOut'));
+
+Cypress.Commands.add('deleteFermentation', (fermentationName, fermentationOwner) => {
+	if (fermentationOwner) {
+		cy.request({
+			method: 'POST',
+			url: '/user/logIn',
+			body: {
+				email: fermentationOwner.email,
+				password: fermentationOwner.password
+			}
+		});
+	}
+	cy.visit('/user/dashboard');
+	cy.get('article.fermentations > ul > li') //
+		.contains(fermentationName)
+		.next('a')
+		.contains('Edit')
+		.click();
+	cy.get('button') //
+		.contains(`Delete ${fermentationName}`)
+		.click();
+});
+
+// Cypress.Commands.add('', () => );
+// Cypress.Commands.add('', () => );
