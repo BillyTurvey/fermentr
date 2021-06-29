e43c4c2eaab6a92196e07f217eb1a6670e271f53
Billy Turvey <billy@billyturvey.com>
2021-06-29 21:35:42 +0100

    Feat: Corrects test which tests redirection after updating device details.

--- cypress/integration/device/editDevice.spec.js
+++ cypress/integration/device/editDevice.spec.js
@@ -26,14 +26,15 @@ describe('Device editing', function () {
 	});
 	it("After updating a device the same device loads in 'view' mode.", function () {
 		cy.logInAs('Jeanette');
+		cy.visit('user/dashboard');
 		cy.get('article.devices > ul > li') //
 			.contains('Raspberry')
 			.next('a')
 			.contains('Edit')
 			.click();
 		cy.get('textarea[name="description"]').type(' 🤙');
-		cy.get('form').contains('Submit').click();
-		cy.location('pathname').should('eq', '/device/60c1b4581d8a36ac2286310a');
+		cy.get('form').contains('Update').click();
+		cy.location('pathname').should('contain', '/device/60c1b4581d8a36ac2286310a');
 	});
 	// it("causes the selected device to be listed as the device's assigned device", function () {
 	// 	cy.get('article.devices > ul > li > a') //
