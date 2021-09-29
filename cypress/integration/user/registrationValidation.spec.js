bc15ce0f6bc16bdeb2c45e323b4a74460bcbbce4
Billy Turvey <billy@billyturvey.com>
2021-09-29 11:50:52 +0100

    Feat: Adds and alters tests 🔴 for including a 'registration password'. This is to protect the system from malicious sign ups when the site is live but still in prototyping phase.

--- cypress/integration/user/registrationValidation.spec.js
+++ cypress/integration/user/registrationValidation.spec.js
@@ -29,6 +29,7 @@ describe('Invalid Form Submission', function () {
 		const sameEmail = newTestEmail();
 		cy.get('input[name="email"]').type(sameEmail);
 		cy.get('input[name="name"]').type('Any Name');
+		cy.get('input[name="registrationPassword"]').type(Cypress.env('registrationPassword'));
 		cy.get('form').contains('Submit').click();
 		cy.get('input[name="email"]').should('have.attr', 'value', sameEmail);
 		cy.get('input[name="name"]').should('have.attr', 'value', 'Any Name');
@@ -37,18 +38,36 @@ describe('Invalid Form Submission', function () {
 		cy.get('input[name="name"]').type("Doesn't Repopulate Passwords");
 		cy.get('input[name="password"]').type('randomP@55word');
 		cy.get('input[name="passwordConfirm"]').type('randomP@55word');
+		cy.get('input[name="registrationPassword"]').type(Cypress.env('registrationPassword'));
 		cy.get('form').contains('Submit').click();
 		cy.get('input[name="password"]').should('be.empty');
 		cy.get('input[name="passwordConfirm"]').should('be.empty');
 	});
 	it('Requires passwords to match', function () {
-		const sameEmail = newTestEmail();
-		cy.get('input[name="email"]').type(sameEmail);
+		cy.get('input[name="email"]').type(newTestEmail());
 		cy.get('input[name="name"]').type('Any Name');
 		cy.get('input[name="password"]').type('randomP@55word');
 		cy.get('input[name="passwordConfirm"]').type('differentP@55word');
+		cy.get('input[name="registrationPassword"]').type(Cypress.env('registrationPassword'));
 		cy.get('form').contains('Submit').click();
 		cy.get('.flash--error').should('contain', 'Passwords do not match.');
+	});
+	it('Requires a signup password for succesful registration.', function () {
+		cy.get('input[name="email"]').type(newTestEmail());
+		cy.get('input[name="name"]').type('Any Name');
+		cy.get('input[name="password"]').type('randomP@55word');
+		cy.get('input[name="passwordConfirm"]').type('randomP@55word');
+		cy.get('form').contains('Submit').click();
+		cy.get('.flash--error').should('contain', 'You must provide a valid registration password.');
+	});
+	it('Requires a valid signup password for succesful registration.', function () {
+		cy.get('input[name="email"]').type(newTestEmail());
+		cy.get('input[name="name"]').type('Any Name');
+		cy.get('input[name="password"]').type('randomP@55word');
+		cy.get('input[name="passwordConfirm"]').type('randomP@55word');
+		cy.get('input[name="registrationPassword"]').type('incorrectRegPW');
+		cy.get('form').contains('Submit').click();
+		cy.get('.flash--error').should('contain', 'You must provide a valid registration password.');
 	});
 });
 
@@ -70,71 +89,9 @@ describe('Email validation', function () {
 			cy.get('input[name="email"]').type(user.email);
 			cy.get('input[name="name"]').type(user.name);
 			cy.get('input[name="password"]').type(Cypress.env('nelsonsPassword'));
+			cy.get('input[name="registrationPassword"]').type(Cypress.env('registrationPassword'));
 			cy.get('input[name="passwordConfirm"]').type(Cypress.env('nelsonsPassword') + '{enter}');
 			cy.get('.flash--error').should('contain', 'Email already registered.');
 		});
 	});
 });
-
-{
-	const temporaryTestUser1 = {
-		name: 'Temporary <br>Tilly',
-		email: 'testymctestface@unreal.com',
-		password: `engine-${Math.random().toString().slice(1, 12)}-float-wireless`
-	};
-
-	describe('Success', function () {
-		it('Redirects to user dashboard after registration and user is logged in', function () {
-			cy.visit('/user/register');
-			cy.get('input[name="email"]').type(temporaryTestUser1.email);
-			cy.get('input[name="name"]').type(temporaryTestUser1.name);
-			cy.get('input[name="password"]').type(temporaryTestUser1.password);
-			cy.get('input[name="passwordConfirm"]').type(temporaryTestUser1.password + '{enter}');
-			cy.location('pathname').should('eq', '/user/dashboard');
-			cy.get('nav').should('contain', `Dashboard`);
-		});
-	});
-
-	describe('Sanitization', function () {
-		before(logOut);
-		it("User's name is escaped", function () {
-			logInAs(temporaryTestUser1);
-			cy.visit('/user/dashboard');
-			cy.get('nav > p').contains('Logged in as Temporary <br>Tilly').should('exist');
-		});
-	});
-
-	describe('Account deletion', function () {
-		it('Account deletion link takes user to a confirmation of deletion page', function () {
-			logInAs(temporaryTestUser1);
-			cy.visit('/user/account');
-			cy.get('a').contains('Delete your account.').click();
-			cy.get('h1').contains('Are you sure you want to delete your account?').should('exist');
-		});
-		it('Deletion confirmation page uses a button to POST the account deletion request to the server', function () {
-			logInAs(temporaryTestUser1);
-			cy.visit('/user/account');
-			cy.get('a').contains('Delete your account.').click();
-			cy.get('form[method="POST"] > button') //
-				.contains('Delete my account forever')
-				.should('exist');
-		});
-		it("User's account is deleted", function () {
-			logInAs(temporaryTestUser1);
-			cy.visit('/user/deleteAccount');
-			cy.get('button').contains('Delete my account forever').click();
-			cy.visit('/user/logIn');
-			cy.get('input[name="email"]').type(temporaryTestUser1.email);
-			cy.get('input[name="password"]').type(temporaryTestUser1.password + '{enter}');
-			cy.get('.flash--error').should('contain', 'Login failed.');
-		});
-		// it('users fermentations are deleted', function () {
-		// 	cy.visit('/user/account');
-		// 	cy.get('input[name="email"]').type(user.email);
-		// 	cy.get('input[name="password"]').type(user.password + '{enter}');
-		// });
-		// it('users devices are deleted', function () {
-		// 	cy.visit('/user/account');
-		// });
-	});
-}
---
+++ cypress/integration/user/successfulRegistration.spec.js
@@ -0,0 +1,65 @@
+import {logInAs, logOut} from '../../fixtures/testUtils.js';
+
+{
+	const temporaryTestUser1 = {
+		name: 'Temporary <br>Tilly',
+		email: 'testymctestface@unreal.com',
+		password: `engine-${Math.random().toString().slice(1, 12)}-float-wireless`
+	};
+
+	describe('Success', function () {
+		it('Redirects to user dashboard after registration and user is logged in', function () {
+			cy.visit('/user/register');
+			cy.get('input[name="email"]').type(temporaryTestUser1.email);
+			cy.get('input[name="name"]').type(temporaryTestUser1.name);
+			cy.get('input[name="registrationPassword"]').type(Cypress.env('registrationPassword'));
+			cy.get('input[name="password"]').type(temporaryTestUser1.password);
+			cy.get('input[name="passwordConfirm"]').type(temporaryTestUser1.password + '{enter}');
+			cy.location('pathname').should('eq', '/user/dashboard');
+			cy.get('nav').should('contain', `Dashboard`);
+		});
+	});
+
+	describe('Sanitization', function () {
+		before(logOut);
+		it("User's name is escaped", function () {
+			logInAs(temporaryTestUser1);
+			cy.visit('/user/dashboard');
+			cy.get('nav > p').contains('Logged in as Temporary <br>Tilly').should('exist');
+		});
+	});
+
+	describe('Account deletion', function () {
+		it('Account deletion link takes user to a confirmation of deletion page', function () {
+			logInAs(temporaryTestUser1);
+			cy.visit('/user/account');
+			cy.get('a').contains('Delete your account.').click();
+			cy.get('h1').contains('Are you sure you want to delete your account?').should('exist');
+		});
+		it('Deletion confirmation page uses a button to POST the account deletion request to the server', function () {
+			logInAs(temporaryTestUser1);
+			cy.visit('/user/account');
+			cy.get('a').contains('Delete your account.').click();
+			cy.get('form[method="POST"] > button') //
+				.contains('Delete my account forever')
+				.should('exist');
+		});
+		it("User's account is deleted", function () {
+			logInAs(temporaryTestUser1);
+			cy.visit('/user/deleteAccount');
+			cy.get('button').contains('Delete my account forever').click();
+			cy.visit('/user/logIn');
+			cy.get('input[name="email"]').type(temporaryTestUser1.email);
+			cy.get('input[name="password"]').type(temporaryTestUser1.password + '{enter}');
+			cy.get('.flash--error').should('contain', 'Login failed.');
+		});
+		// it('users fermentations are deleted', function () {
+		// 	cy.visit('/user/account');
+		// 	cy.get('input[name="email"]').type(user.email);
+		// 	cy.get('input[name="password"]').type(user.password + '{enter}');
+		// });
+		// it('users devices are deleted', function () {
+		// 	cy.visit('/user/account');
+		// });
+	});
+}
