694e710ad6cfd97031069bae07b006013110a7af
Billy Turvey <billy@billyturvey.com>
2021-11-05 21:36:30 +0000

    Feat: Adds d3.js code to draw the graph, almost working, circles/points are in the inspector but not visible on screen.

--- public/scripts/drawGraph.js
+++ public/scripts/drawGraph.js
@@ -1,13 +1,45 @@
-// import * as d3 from 'https://cdn.skypack.dev/d3@7';
+import * as d3 from 'https://cdn.skypack.dev/d3@7';
+
+async function drawGraph() {
+	const idRegEx = /[a-z0-9]{24}/; //24 lowercase letter or number characters, aka mongoose
+	const fermentationId = idRegEx.exec(window.location.pathname)[0];
+	let graphData = await fetch(`/api/${fermentationId}/graph`).catch(error => {
+		console.error(error.message);
+		// informUserFetchRequestFailed();
+	});
+	graphData = await graphData.json();
+
+	const sum = (previous, current) => previous + current[1];
+	const meanTemp = graphData.reduce(sum, 0) / graphData.length;
+
+	const startTime = graphData[0][0];
+	const endTime = graphData[graphData.length - 1][0];
+	const timeRange = endTime - startTime;
+	const graph = document.getElementById('temp-graph');
+	graph.setAttribute('viewBox', `0 0 ${timeRange * 1.1} ${500}`);
+
+	const svg = d3
+		.select('#temp-graph')
+		.selectAll('circle')
+		.data(graphData)
+		.enter()
+		.append('circle')
+		.attr('cx', d => d[0] - startTime) //time
+		.attr('cy', d => d[1] * 10) //temp
+		.attr('r', 4000)
+		.attr('fill', d => `hsl(${300 + (d - 18) * 20}, 80%, 80%)`);
+
+	console.log(svg);
+}
+
+drawGraph();
 
 //ASAP a fetch request should grap an overview of the fermentation's thermal profile to date AND the target thermal profile if it has one too
 // if there's a thermal profile, the graph width should be the length of the planned fermentation length
 // if the fermentation has no planned profile the width of the graph should be the width of the time elapsed
 // the graph should be the full width of the useable screen
 
-async function drawGraph() {
-	//
-	/*
+/*
 		Thinking out loud...
 		we need the array of time series thermal data to draw the graph. Easy enough to fetch.
 
@@ -26,23 +58,3 @@ async function drawGraph() {
 			- if there are more pixels than data then the 
 			- for each pixel work out the mean of all the temps measured during that time period, this gives a new array of y values for each pixel.
 	*/
-}
-
-function d3Something() {
-	// select this svg and put it into this variable
-	const svg = d3.select('#actualTempGraph'); // <svg id='actualTempGraph'></svg>
-	const points = svg.selectAll();
-}
-
-async function fetchTempData() {
-	const graphWidth = document.getElementById('temp-graph').width;
-
-	const idReg = /[a-z0-9]{24}/;
-	const fermentationId = idReg.exec(window.location.pathname)[0];
-	const graphData = await fetch(`/api/${fermentationId}/graph`).catch(error => {
-		console.error(error.message);
-		// informUserFetchRequestFailed();
-	});
-}
-
-fetchTempData();
