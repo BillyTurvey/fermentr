daa633c6bd9eaeac7bcf9ba932029892d68807e1
Billy Turvey <billy@billyturvey.com>
2021-11-08 16:37:02 +0000

    Feat: Cleans up pseudo code and notes from drawGraph.js

--- public/scripts/drawGraph.js
+++ public/scripts/drawGraph.js
@@ -71,7 +71,7 @@ async function drawGraph() {
 		.attr('cx', d => (d[0] - startTime) / (timeRange / graphWidth)) //time
 		.attr('cy', d => convertTempToYAxisValue(d[1])) //temp
 		.attr('r', 2)
-		.attr('fill', d => `hsl(${120 - (d[1] - 20) * 20}, 100%, 40%)`)
+		.attr('fill', d => `hsl(${120 - (d[1] - 19.8) * 30}, 100%, 40%)`)
 		.attr('stroke', null);
 }
 
@@ -96,31 +96,3 @@ function getMinMax(logArr) {
 		{min: logArr[0][1], max: logArr[0][1]}
 	);
 }
-
-// const sum = (previous, current) => previous + current[1];
-// const meanTemp = graphData.reduce((previous, current) => {previous + current[1]}, 0) / graphData.length;
-
-//ASAP a fetch request should grap an overview of the fermentation's thermal profile to date AND the target thermal profile if it has one too
-// if there's a thermal profile, the graph width should be the length of the planned fermentation length
-// if the fermentation has no planned profile the width of the graph should be the width of the time elapsed
-// the graph should be the full width of the useable screen
-
-/*
-		Thinking out loud...
-		we need the array of time series thermal data to draw the graph. Easy enough to fetch.
-
-		To draw the graph:
-		- Work out the overal scale:
-			- The x axis total length will vary depending on how much data has been collected. Perhaps it will be 110%? of the length of the data recorded. x = 0 = yeast pitch time || the first temp measurement
-			- The data line should be in the middle of the y axis. 
-				- The middle of the y axis should be the mean y value of all datapoints
-			- The range of the data shown on the y axis is to be determined, try either 0-50•C or the range of the y values +- 100-200% either side? 
-
-		- Draw a rectangle to fill the alloted space on the page
-			- Y max = y * 1.2
-
-		- Problem: Data can not assumed to be equally distanced along the x axis, the device may have been unplugged for a short while or it may not have been able to connect to the wifi. Data are mostly one minute apart but not always.
-			- grab the range in time and devide it by the number of pixels available in the x axis in the alloted area to get the time period that each pixel width on the x axis represents.
-			- if there are more pixels than data then the 
-			- for each pixel work out the mean of all the temps measured during that time period, this gives a new array of y values for each pixel.
-	*/
