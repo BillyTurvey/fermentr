7e206e03cb7253ea6b63adfccb7f4779fde42cc8
Billy Turvey <billy@billyturvey.com>
2021-11-25 11:11:24 +0000

    Feat: Clicking on the 'fermentr' logo redirects to dashboard if user is logged in.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 import {Router} from 'express';
 const router = Router();
 
+router.get('/', (req, res, next) => {
+	if (req.user) return res.redirect('/user/dashboard');
+	next();
+});
+
 router.use(function removeSessionFromRequest(req, res, next) {
 	req.session = null;
 	next();
