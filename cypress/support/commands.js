238bb550f305fede5aecfb990b996530cb62688e
Billy Turvey <billy@billyturvey.com>
2022-01-21 12:18:39 +0000

    Feat: Alters test util createDevice() so that it navigates to the newly created device's page so that the url can be grabbed and used in tests more easily.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 import jeanette from '../fixtures/testUserJeanette.json';
 import nelson from '../fixtures/testUserNelson.json';
 
-function logInAs(user) {
+Cypress.Commands.add('logInAs', user => {
 	if (typeof user === 'string') {
 		if (user === 'Jeanette') {
 			user = jeanette;
@@ -18,9 +18,7 @@ function logInAs(user) {
 	cy.visit('/user/logIn');
 	cy.get('input[name="email"]').type(user.email);
 	cy.get('input[name="password"]').type(password + '{enter}');
-}
-
-Cypress.Commands.add('logInAs', logInAs);
+});
 
 Cypress.Commands.add('logOut', () => cy.request('POST', '/user/logOut'));
 
@@ -51,3 +49,22 @@ Cypress.Commands.add('deleteDevice', (deviceName, deviceOwner) => {
 		.contains(`Delete ${deviceName}`)
 		.click();
 });
+
+Cypress.Commands.add('createDevice', (deviceName, description) => {
+	cy.visit('/device/add');
+	cy.get('input[name="name"]').type(deviceName);
+	cy.get('textarea[name="description"]').type(description || 'Temporary test device.');
+	cy.get('form').contains('Submit').click();
+	cy.get('a').contains(deviceName).click(); //allows tests to save the url of the freshly created device
+});
+
+Cypress.Commands.add('createFermentation', (fermentationName, description) => {
+	cy.visit('/fermentation/add');
+	cy.get('input[name="name"]').type(fermentationName);
+	cy.get('textarea[name="description"]').type(description || 'Temporary test fermentation.');
+	cy.get('form').contains('Submit').click();
+});
+
+export const newTestDeviceName = () => `TemporaryTestDevice${Math.random().toString().slice(2, 7)}`;
+export const newTestFermentationName = () =>
+	`TemporaryTestFermentation${Math.random().toString().slice(2, 7)}`;
