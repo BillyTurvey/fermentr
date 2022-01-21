3f459492b1c3114d26fddb2f0968c35bc7663450
Billy Turvey <billy@billyturvey.com>
2022-01-21 10:18:54 +0000

    Feat: Removes dependence on pre-existing DB state for test: 'Device can be assigned to a fermentation when a user adds a fermentation to their account'. Both the fermentation and device used in the test are created and deleted within the test its self.

--- cypress/integration/device/deviceAssignment.spec.js
+++ cypress/integration/device/deviceAssignment.spec.js
@@ -2,29 +2,39 @@ describe('Device Assignment', function () {
 	beforeEach(() => {
 		cy.logInAs('Jeanette');
 	});
-	it('Device can be assigned to a fermentation when a user adds a fermentation to their account.', function () {
+	it.only('Device can be assigned to a fermentation when a user adds a fermentation to their account.', function () {
 		const sameTestFermentationName = `TemporaryTestFermentation${Math.random().toString().slice(2, 7)}`;
+		const sameTestDeviceName = `TemporaryTestDevice${Math.random().toString().slice(2, 7)}`;
+		// create device
+		cy.visit('/device/add');
+		cy.get('input[name="name"]').type(sameTestDeviceName);
+		cy.get('form').contains('Submit').click();
+		// create fermentation
 		cy.visit('/fermentation/add');
 		cy.get('input[name="name"]').type(sameTestFermentationName);
-		cy.get('input[id="Persistent Test Device Enid"]').click();
+		// select device
+		cy.get(`input[id="${sameTestDeviceName}"]`).click();
 		cy.get('textarea[name="description"]').type(
 			'This temporary test fermentation is used to make sure the correct documents are updated when a user assigns a device to a fermentation during the process of registering a new fermentation.'
 		);
 		cy.get('form > button').contains('Submit').click();
+		// assert device is assigned to fermentation on fermentation page
 		cy.get('p')
-			.contains(
-				`Device 'Persistent Test Device Enid' is currently assigned to '${sameTestFermentationName}'.`
-			)
+			.contains(`Device '${sameTestDeviceName}' is currently assigned to '${sameTestFermentationName}'.`)
 			.should('exist');
-		cy.get('a').contains('Persistent Test Device Enid').click();
+		// assert device is assigned to fermentation on device page
+		cy.get('a').contains(`${sameTestDeviceName}`).click();
 		cy.get('p')
-			.contains(`Persistent Test Device Enid is currently assigned to '${sameTestFermentationName}'.`)
+			.contains(`${sameTestDeviceName} is currently assigned to '${sameTestFermentationName}'.`)
 			.should('exist');
+		// delete fermentation
 		cy.get('a').contains(sameTestFermentationName).click();
-		cy.get('a').contains('Edit fermentation details.').click();
+		cy.get('a').contains('Edit fermentation details').click();
 		cy.get('button') //
 			.contains(`Delete ${sameTestFermentationName}`)
 			.click();
+		// delete device
+		cy.deleteDevice(sameTestDeviceName);
 	});
 
 	it('Device can be assigned to a fermentation when a user edits a fermentation.', function () {
