0c80f2d70f6df8f9f16d138817e57cf0291d6db9
Billy Turvey <billy@billyturvey.com>
2022-01-23 10:16:26 +0000

    Feat: Adds flash message to confirm device deletion.

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -18,16 +18,14 @@ export const hashKey = (req, res, next) => {
 
 export const addToDatabase = async (req, res) => {
 	try {
-		// pre save middleware on the device model...
-		//// ...adds the device id to the fermentation document
-		//// ...adds the device id to the user document
 		const device = await Device.create({
 			deviceID: res.locals.deviceID,
 			name: req.body.name,
 			description: req.body.description,
 			keyHash: res.locals.keyHash,
 			owner: req.user._id,
-			assignedFermentation: req.body.assignedFermentation === 'null' ? null : req.body.assignedFermentation
+			assignedFermentation:
+				req.body.assignedFermentation === 'null' ? null : req.body.assignedFermentation
 		});
 		req.flash('success', 'Device registered.');
 		return res.render('device/addDevice', {
@@ -52,10 +50,6 @@ export const addToDatabase = async (req, res) => {
 
 export const update = async (req, res) => {
 	try {
-		// pre save middleware on the device model...
-		//// ...adds the device id to the fermentation document
-		//// ...adds the device id to the user document
-
 		const device = req.device;
 		device.name = req.body.name;
 		device.description = req.body.description;
@@ -90,7 +84,6 @@ export const addDeviceForm = async (req, res, next) => {
 };
 
 export const editDeviceForm = async (req, res, next) => {
-	// if (!req.device) res.status()
 	res.render('device/editDevice', {
 		title: `Edit ${req.device.name}`,
 		device: req.device,
@@ -99,9 +92,9 @@ export const editDeviceForm = async (req, res, next) => {
 };
 
 export const deleteDevice = async function (req, res, next) {
-	// pre delete hook on the device model removes the device id from the user's db entry
 	try {
 		await Device.findByIdAndDelete(req.device._id).exec();
+		req.flash('success', `Device: ${req.device.name} was successfully deleted.`);
 		res.redirect('/user/dashboard');
 	} catch (error) {
 		console.error(error);
@@ -118,7 +111,6 @@ export const view = async (req, res) => {
 
 export const authenticateAndAttachToReq = async (req, res, next, id) => {
 	if (req.user && (await req.user.ownsDevice(id))) {
-		//user is logged in and is trying to view or edit device they own
 		try {
 			const device = await Device.findById(id).populate('assignedFermentation').exec();
 			req.device = device;
@@ -128,7 +120,6 @@ export const authenticateAndAttachToReq = async (req, res, next, id) => {
 			res.status(401).end();
 		}
 	} else if (req.header('device-key')) {
-		// device posting data to be logged
 		try {
 			const device = await Device.findById(id).populate('assignedFermentation').exec();
 			const key = req.header('device-key');
@@ -167,7 +158,6 @@ export const logReading = async (req, res, next) => {
 				'This device is not assigned to a fermentation, therefore the data it is submitting cannot be saved.'
 			);
 		}
-		//Populate the fermentation when the device is retreived from the database?
 		try {
 			const dataLog = await DataLog.findById(fermentation.dataLog).exec();
 			dataLog.thermalProfile.actual.push({
@@ -175,7 +165,6 @@ export const logReading = async (req, res, next) => {
 				temp: req.body.temperature
 			});
 			dataLog.save();
-			// res.status(200).end();
 			res.status(200).json({
 				message: 'Thank you! Your reading has been saved to the database.'
 			});
