ef46afe98199b197322255745909acaa1f73a1c3
Billy Turvey <billy@billyturvey.com>
2022-01-24 13:02:02 +0000

    Fix: Removes redundant comments from www.js

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+import app from '../app.js';
+import debug from 'debug';
+import http from 'http';
 import mongoose from 'mongoose';
 
-// Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DB, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
@@ -13,60 +15,25 @@ mongoose.connection.on('error', err => {
 	console.error(`  ❌  ❌  ${err.message}  ❌  ❌  `);
 });
 
-/**
- * Module dependencies.
- */
-
-import app from '../app.js';
-import debug from 'debug';
-import http from 'http';
-
-// debug = debug('fermentr-app:server');
-
-/**
- * Get port from environment and store in Express.
- */
-
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
 var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
-
 function normalizePort(val) {
 	var port = parseInt(val, 10);
-
 	if (isNaN(port)) {
-		// named pipe
 		return val;
 	}
-
 	if (port >= 0) {
-		// port number
 		return port;
 	}
-
 	return false;
 }
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
 	if (error.syscall !== 'listen') {
@@ -89,10 +56,6 @@ function onError(error) {
 			throw error;
 	}
 }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
 	var addr = server.address();
